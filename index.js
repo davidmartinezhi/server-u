6fe704dfd127d79c26a55c155001e37635324f68
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-09-30 14:20:19 -0500

    Importaciones iniciales

---
+++ index.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose"); //importado mongoose
+const app = require("./app"); //importado app
+const port = process.allowedNodeEnvironmentFlags.PORT || 3977;  //Puerto de nuestra aplicación
+const portDb = 27017; //Puerto de la base de datos
+const { API_VERSION } = require("./config");
