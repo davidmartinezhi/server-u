5ecb2b825e7f978fe3c5ac4b6ad131e2a697ee85
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-09-30 14:25:28 -0500

    Importado el port de la base de datos

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 const API_VERSION = "v1";
-const IP_SERVER = "localhost";
+const IP_SERVER = "localhost";  //Ip del servidor
+const PORT_DB = 27017; //Puerto de la base de datos
 
 module.exports = {
     API_VERSION,
-    IP_SERVER
+    IP_SERVER,
+    PORT_DB
 }
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose"); //importado mongoose
 const app = require("./app"); //importado app
 const port = process.allowedNodeEnvironmentFlags.PORT || 3977;  //Puerto de nuestra aplicación
-const portDb = 27017; //Puerto de la base de datos
-const { API_VERSION, IP_SERVER } = require("./config");
+const { API_VERSION, IP_SERVER, PORT_DB } = require("./config");
 
 //Conexión a base de datos
-mongoose.connect(`mongodb://`);
+mongoose.connect(`mongodb://${IP_SERVER}:${PORT_DB}/davidgerardomartinez`);
