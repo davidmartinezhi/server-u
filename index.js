2ecaf6da8f2b1424cf695e95c91e306e7dbbcca1
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-09-30 14:46:29 -0500

    Conexión a la base de datos terminada

--- index.js
+++ index.js
@@ -4,4 +4,17 @@ const port = process.allowedNodeEnvironmentFlags.PORT || 3977;  //Puerto de nues
 const { API_VERSION, IP_SERVER, PORT_DB } = require("./config");
 
 //Conexión a base de datos
-mongoose.connect(`mongodb://${IP_SERVER}:${PORT_DB}/davidgerardomartinez`);
+mongoose.connect(`mongodb://${IP_SERVER}:${PORT_DB}/davidgerardomartinez`,
+ { useNewUrlParser: true}, ( err, res) => {
+     if(err){
+         throw err;
+     } else{
+         console.log("La conexión a la base de datos fue exitosa");
+         app.listen(port, () => {
+            console.log("#####################");
+            console.log("##### API REST ######");
+            console.log("#####################");
+         })
+        
+     }
+ });
