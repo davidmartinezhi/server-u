92e287ec1bb80e7219a5527d63569d1214c59a00
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-09-30 14:16:44 -0500

    Agregada documentación

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const app = express();
+const app = express();  //Iniciacion de express
 const { API_VERSION } = require("./config");
 
 //Load routings
 //...
 
+//Configuracion de bodyParser en express
 //app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(bodyParser.json());
 
