962495474c9853f188b167db1c7dff1aac6cafdd
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-09-30 21:59:35 -0500

    Conexión correcta

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const mongoose = require("mongoose"); //importado mongoose
+const mongoose = require('mongoose'); //importado mongoose
 const app = require("./app"); //importado app
-const port = process.allowedNodeEnvironmentFlags.PORT || 3977;  //Puerto de nuestra aplicación
+const port = process.env.PORT || 3977;  //Puerto de nuestra aplicación
 const { API_VERSION, IP_SERVER, PORT_DB } = require("./config");
 
 //Conexión a base de datos
-mongoose.connect(`mongodb://${IP_SERVER}:${PORT_DB}/davidgerardomartinez`,
- { useNewUrlParser: true}, ( err, res) => {
-     if(err){
-         throw err;
-     } else{
-         console.log("La conexión a la base de datos fue exitosa");
-         app.listen(port, () => {
-            console.log("#####################");
-            console.log("##### API REST ######");
-            console.log("#####################");
-         })
-        
-     }
- });
+mongoose.connect(
+  `mongodb://${IP_SERVER}:${PORT_DB}/davidgerardomartinez`,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, res) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log("La conexión a la base de datos fue exitosa");
+
+      app.listen(port, () => {
+        console.log("#####################");
+        console.log("##### API REST ######");
+        console.log("#####################");
+        console.log(`http://${IP_SERVER}:${port}/api/${API_VERSION}/`);
+      });
+    }
+  }
+);
