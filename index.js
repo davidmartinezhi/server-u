654e0a848703e5504c67b6daa293ce3ea70df641
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-12-22 22:05:30 -0600

    Setted useFindAndModify to false

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = require("./app"); //importado app
 const port = process.env.PORT || 3977;  //Puerto de nuestra aplicación
 const { API_VERSION, IP_SERVER, PORT_DB } = require("./config");
 
+mongoose.set("useFindAndMosify", false);
+
 //Conexión a base de datos
 mongoose.connect(
   `mongodb://${IP_SERVER}:${PORT_DB}/davidgerardomartinez`,
