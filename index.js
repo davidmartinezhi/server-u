e6d6374ea4ae01c97710d197a0a27cd807969222
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-02 19:17:41 -0600

    Comentado el set de useAndModify para que funcione nodemon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require("./app"); //importado app
 const port = process.env.PORT || 3977;  //Puerto de nuestra aplicación
 const { API_VERSION, IP_SERVER, PORT_DB } = require("./config");
 
-mongoose.set("useFindAndMosify", false);
+//mongoose.set("useFindAndModify", false);
 
 //Conexión a base de datos
 mongoose.connect(
