f845a3761ab9c46d69fb6c22537a3614db2c7e66
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-02 19:18:25 -0600

    Utilizado el paquete prettier en el codigo

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const API_VERSION = "v8";
+const API_VERSION = "v1";
 const IP_SERVER = "localhost";  //Ip del servidor
 const PORT_DB = 27017; //Puerto de la base de datos
 
--- models/user.js
+++ models/user.js
@@ -2,16 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = Schema({
-    name: String,
-    lastname: String,
-    email: {
-        type: String,
-        unique: true
-    },
-    password: String,
-    role: String,
-    active: Boolean
-
+  name: String,
+  lastname: String,
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: String,
+  role: String,
+  active: Boolean,
 });
 
 module.exports = mongoose.model("User", UserSchema);
