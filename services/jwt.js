b46e1eb32370ada541aefab181dd94f7ecd5db49
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-12 19:28:02 -0600

    Creada contraseña

--- services/jwt.js
+++ services/jwt.js
@@ -1,2 +1,6 @@
 const jwt = require("jwt-simple");
 const moment = require("moment");
+
+//Aquí genero clave secreta que solo debo de ver yo
+const SECRET_KEY = "gR7cH9Svfj8JLe4c186Ghs48hheb3902nh5DsA";
+
