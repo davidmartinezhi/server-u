a580b1e10bb5ec3ec2baddc0dc7750d7e6c3d5da
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-22 13:17:57 -0600

    Creada ruta para refrescar el access token

---
+++ routers/auth.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const AuthController = require("../controllers/auth");
+
+//Para generar rutas
+const api = express.Router();
+
+api.post("refresh-access-token", AuthController.refreshAccessToken);
+
+module.exports = {
+    api
+}
