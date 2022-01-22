60255150433bc04d7d04ca40e36521d7939c2775
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-22 13:13:09 -0600

    Creado token para checar fecha de expiración

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,16 @@
 const jwt = require("../services/jwt");
 const moment = require("moment");
 const user = require("../models/user");
+
+//Checar fecha de expiración también en server para mayor seguridad
+function willExpireToken( token ) {
+    const {exp} = jwt.decodedToken(token);
+    const currentDate = moment.unix();
+
+    if(currentDate > exp){
+        return true;
+    }
+
+    return false;
+}
+
