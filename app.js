64a9d556f4de0f4d8f10da0e86d7da6abf99eddb
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-22 13:21:07 -0600

    Endpoint de refreshToken terminado

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();  //Iniciacion de express
 const { API_VERSION } = require("./config");
 
 //Load routings
+const authRoutes = require("./routers/auth");
 const userRoutes = require("./routers/user");
 
 //Configuracion de bodyParser en express, investigar que es este codigo. Para que funciona
@@ -14,6 +15,7 @@ app.use(bodyParser.json());
 //Configure header HTTP
 
 //Router basic
+app.use(`/api/${API_VERSION}`, authRoutes);
 app.use(`/api/${API_VERSION}`, userRoutes);
 
 module.exports = app;
