3fdaa2790e0137065b330bfa59e519dfd82431fc
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-22 14:19:14 -0600

    refrshAccessToken funcional

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const jwt = require("../services/jwt");
 const moment = require("moment");
-const user = require("../models/user");
+const User = require("../models/user");
+const { use } = require("../routers/auth");
 
 //Checar fecha de expiración también en server para mayor seguridad
 function willExpireToken( token ) {
@@ -16,7 +17,34 @@ function willExpireToken( token ) {
 
 //refreshAccessToken
 function refreshAccessToken(req, res){
-    console.log("Estamos refrecando el access token.");
+    
+    const {refreshToken} = req.body;    //Guarda el Token que viene en el body
+    
+    const isTokenExpired = willExpireToken(refreshToken);
+
+    if(isTokenExpired){
+        res.status(404).send({message: "Refresh Token ha expirado."});
+    }
+    else{
+        const { id } = jwt.decodedToken(refreshToken);
+
+        use.findOne({_id: id}, (err, userStored) => {
+            if(err){
+                res.status(500).send({message: "Error del servidor."});
+            }
+            else{
+                if(!userStored){
+                    res.status(404).send({message:"Usuario no encontado"});
+                }
+                else{
+                    res.status(200).send({
+                        accessToken: jwt.createAccessToken(userStored),
+                        refreshToken: refreshToken
+                    });
+                }
+            }
+        });
+    }
 }
 
 module.exports = {
