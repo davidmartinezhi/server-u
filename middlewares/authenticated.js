95798723d13afc292ff756fb23337176cf3a7852
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-27 20:20:06 -0600

    Token authentication

---
+++ middlewares/authenticated.js
@@ -0,0 +1,27 @@
+const jwt = require("jwt-simple");
+const moment = require("moment");
+
+const SECRET_KEY = "gR7cH9Svfj8JLe4c186Ghs48hheb3902nh5DsA";
+
+exports.ensureAuth = (req, res) => {
+  if (!req.headers.autorization) {
+    return res
+      .status(403)
+      .send({ message: "La petición no tiene cabezera de autentificación." });
+  }
+
+  const token = req.headers.autorization.replace(/['"]+/g, "");
+
+  try {
+    const payload = jwt.decode(token, SECRET_KEY);
+    if (payload.exp <= moment.unix) {
+      return res.status(404).send({ message: "El Token ha expirado." });
+    } 
+  } catch (ex) {
+    //console.log(ex);
+    res.status(404).send({message: "Token Invalido."});
+  }
+
+  req.user = payload;
+  next();
+};
