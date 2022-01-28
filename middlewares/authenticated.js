b33ffe66a41483c4de501cf9a5e6ef5cd79cbe11
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-27 20:30:45 -0600

    Bug de unix resuelto

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -3,18 +3,18 @@ const moment = require("moment");
 
 const SECRET_KEY = "gR7cH9Svfj8JLe4c186Ghs48hheb3902nh5DsA";
 
-exports.ensureAuth = (req, res) => {
-  if (!req.headers.autorization) {
+exports.ensureAuth = (req, res, next) => {
+  if (!req.headers.authorization) {
     return res
       .status(403)
       .send({ message: "La petición no tiene cabezera de autentificación." });
   }
 
-  const token = req.headers.autorization.replace(/['"]+/g, "");
+  const token = req.headers.authorization.replace(/['"]+/g, "");
 
   try {
-    const payload = jwt.decode(token, SECRET_KEY);
-    if (payload.exp <= moment.unix) {
+    var payload = jwt.decode(token, SECRET_KEY);
+    if (payload.exp <= moment.unix()) {
       return res.status(404).send({ message: "El Token ha expirado." });
     } 
   } catch (ex) {
