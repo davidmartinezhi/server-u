e2ec0a3a3aa9cd32f2bd718a78ec6a2db6f8aa6b
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-28 18:45:56 -0600

    configurado el header

--- app.js
+++ app.js
@@ -12,7 +12,17 @@ const userRoutes = require("./routers/user");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//Configure header HTTP
+// Configure Header HTTP
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
 
 //Router basic
 app.use(`/api/${API_VERSION}`, authRoutes);
