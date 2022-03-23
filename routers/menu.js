01e4d72af0da232c241230a27a2190cea113272a
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-03-23 17:41:13 -0600

    Creada ruta para recuperar los menus

--- routers/menu.js
+++ routers/menu.js
@@ -6,7 +6,12 @@ const md_auth = require("../middlewares/authenticated");
 //Inicío la api con express
 const api = express.Router();
 
+//Crea menu en la base de datos
 api.post("/add-menu", [md_auth.ensureAuth], MenuController.addMenu);
+
+//Despliega menú desde la base de datos al front end
+api.get("/menus", [md_auth.ensureAuth], MenuController.getMenus);
+
 
 //Todas las rutas se exportan automaticamente, de esta manera
 module.exports = api;
