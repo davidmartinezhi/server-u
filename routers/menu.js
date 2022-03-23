358c3d0b9e102794ea4678604a64cd0bfd7c4d3b
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-03-22 19:15:29 -0600

    Creada ruta para los menus

---
+++ routers/menu.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const MenuController = require("../controllers/menu");
+const md_auth = require("../middlewares/authenticated");
+
+
+//Inicío la api con express
+const api = express.Router();
+
+api.post("/add-menu", [md_auth], MenuController.addMenu);
+
+//Todas las rutas se exportan automaticamente, de esta manera
+module.exports = api;
