13a82a11f77c2f72dd6df5a2be85cc2adb0a37be
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-03-22 19:24:03 -0600

    Arreglado autentificación

--- routers/menu.js
+++ routers/menu.js
@@ -6,7 +6,7 @@ const md_auth = require("../middlewares/authenticated");
 //Inicío la api con express
 const api = express.Router();
 
-api.post("/add-menu", [md_auth], MenuController.addMenu);
+api.post("/add-menu", [md_auth.ensureAuth], MenuController.addMenu);
 
 //Todas las rutas se exportan automaticamente, de esta manera
 module.exports = api;
