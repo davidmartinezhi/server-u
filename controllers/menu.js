d7f5a5959f350520b3b31c15f739df8bb858c2bb
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-03-23 17:31:31 -0600

    Endpoint para crear menu, terminado

--- controllers/menu.js
+++ controllers/menu.js
@@ -1,9 +1,34 @@
-const menu = require("../models/menu");
+const Menu = require("../models/menu");
 
-function addMenu(req, res){
-    console.log("Add menu");
+function addMenu(req, res) {
+  //Crea nuevos menus en la base de datos
+
+  const { title, url, order, active } = req.body;
+
+  //Creando modelo de menú vacío
+  const menu = new Menu();
+
+  //Rellenando el menu por lo recibido en el request
+  menu.title = title;
+  menu.url = url;
+  menu.order = order;
+  menu.active = active;
+
+  //Guardar el menu
+  menu.save((err, createMenu) => {
+    if (err) {
+      //Si no lo crea y marca error
+      res.status(500).send({ message: "Error del servidor" });
+    } else {
+      if (!createMenu) {
+        res.status(404).send({ message: "Error al crear el menú" });
+      } else {
+        res.status(200).send({ message: "Menú creado correctamente" });
+      }
+    }
+  });
 }
 
 module.exports = {
-    addMenu
+  addMenu,
 };
