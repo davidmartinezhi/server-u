4bd920bc38e206fc0177574368aad64552b91bb3
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-19 16:58:20 -0500

    Creado controlador para modelo newsletter

---
+++ controllers/newsletter.js
@@ -0,0 +1,9 @@
+const Newsletter = require('../models/newsletter');
+
+function subscribeEmail(req, res){
+    console.log("Subscribe email ok....");
+}
+
+module.exports = {
+    subscribeEmail,
+}
