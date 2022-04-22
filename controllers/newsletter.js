9020e009ff8e8811b0fd7f8f64676393cc024e48
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-22 15:19:22 -0500

    registro de email correcto

--- controllers/newsletter.js
+++ controllers/newsletter.js
@@ -1,9 +1,36 @@
-const Newsletter = require('../models/newsletter');
+const Newsletter = require("../models/newsletter");
 
-function subscribeEmail(req, res){
-    console.log("Subscribe email ok....");
+//Subcribe a newsletter
+function subscribeEmail(req, res) {
+  //console.log(req.params);
+
+  const email = req.params.email;
+
+  const newsletter = new Newsletter();
+
+  if (!email) {
+    res.status(404).send({ code: 404, message: "El email es obligatorio." });
+  } else {
+    newsletter.email = email.toLowerCase();
+
+    newsletter.save((err, newsletterStored) => {
+      if (err) {
+        res.status(500).send({ code: 500, message: "Email ya registrado." });
+      } else {
+        if (!newsletter) {
+          res
+            .status(404)
+            .send({ code: 404, message: "Error al registrar correo." });
+        } else {
+          res
+            .status(200)
+            .send({ code: 200, message: "Email registrado correctamente." });
+        }
+      }
+    });
+  }
 }
 
 module.exports = {
-    subscribeEmail,
-}
+  subscribeEmail,
+};
