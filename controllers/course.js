e1b687559cc9a439298d4c76ed79c480cd196384
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-25 09:58:44 -0500

    Creada api para añadir cursos a la base de datos

--- controllers/course.js
+++ controllers/course.js
@@ -1,7 +1,26 @@
 const Course = require("../models/course");
 
 function addCourse( req, res ){
-    console.log("Creando curso...");
+
+    const body = req.body;
+    const course = new Course(body);
+    course.order = 1000;
+
+
+
+    course.save((err, courseStored) => {
+        if(err){
+            res.status(400).send({code: 400, message: "El curso que estas creando ya existe."});
+        }
+        else{
+            if(!courseStored){
+                res.status(400).send({code: 400, message: "No se ha podido crear el curso."});
+            }
+            else{
+                res.status(200).send({ code: 200, message: "Curso creado correctamente."});
+            }
+        }
+    });
 }
 
 module.exports = {
