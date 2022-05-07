6e680658fc73479a0eacade7ca0c1662592b1b9b
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-07 17:28:57 -0500

    Creada api para crear post

--- controllers/post.js
+++ controllers/post.js
@@ -1,7 +1,22 @@
 const Post = require("../models/post");
 
 function addPost(req, res) {
-    console.log("Todo bien...");
+    const body = req.body;
+    const post = new Post(body);
+
+    post.save((err, postStored) => {
+        if(err){
+            res.status(500).send({ code: 500, message: "Error del servidor."});
+        }
+        else{
+            if(!postStored){
+                res.status(400).send({cose:400, message: "No se ha podido crear el post"});
+            }
+            else{
+                res.status(200).send({code: 200, message: "Post creado correctamente"});
+            }
+        }
+    });
 }
 
 module.exports = {
