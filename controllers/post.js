815b569049ad4664041210cf982321119b8cc507
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-07 17:41:11 -0500

    Creada paginación

--- controllers/post.js
+++ controllers/post.js
@@ -20,7 +20,28 @@ function addPost(req, res) {
 }
 
 function getPost(req, res) {
-    console.log("getpostt...");
+    
+    const { page = 1, limit = 10} = req.query; //Esto es vuando ponemos en el lin ?page=1&limit=10...
+
+    const options = {
+        page,
+        limit: parseInt(limit), //Esto se usa para cuando debemos enviar entero
+        sort: {date: "desc" }
+    }
+
+    Post.paginate({}, options, (err, postsStored) => {
+        if(err){
+            res.status(500).send({code: 500, message: "Error del servidor."});
+        }
+        else{
+            if(!postsStored){
+                res.status(404).send({code: 404, message: "No se ha encontrado ningún post,"});
+            }
+            else{
+                res.status(200).send({code: 200, posts: postsStored});
+            }
+        }
+    });
 }
 
 module.exports = {
